4fba10817f140bad9ef5998a2e432df17f132b3b
M4h45amu7x <89692002+M4h45amu7x@users.noreply.github.com>
2021-08-29 19:42:20 +0700

    Add files via upload

---
+++ sql/webshop.sql
@@ -0,0 +1,86 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 29, 2021 at 02:42 PM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 8.0.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `webshop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `goods`
+--
+
+CREATE TABLE `goods` (
+  `id` int(11) NOT NULL,
+  `name` longtext NOT NULL,
+  `display_name` text NOT NULL,
+  `description` text DEFAULT NULL,
+  `price` float NOT NULL,
+  `image` text NOT NULL,
+  `rcon_host` text NOT NULL DEFAULT '\'127.0.0.1\'',
+  `rcon_port` int(5) NOT NULL,
+  `rcon_password` text NOT NULL,
+  `command` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `goods`
+--
+
+INSERT INTO `goods` (`id`, `name`, `display_name`, `description`, `price`, `image`, `rcon_host`, `rcon_port`, `rcon_password`, `command`) VALUES
+(1, '1xStone', 'Stone 1 ชิ้น', 'Stone 1 ชิ้น', 10, '1.jpg', '127.0.0.1', 25575, '12341234', 'give %player% minecraft:stone 1'),
+(2, 'diamond64', 'Diamond 64 ชิ้น', 'Diamond 64 ชิ้น', 10, '1.jpg', '127.0.0.1', 25575, '12341234', 'give %player% minecraft:diamond 64');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `history`
+--
+
+CREATE TABLE `history` (
+  `username` text NOT NULL,
+  `goods` int(11) NOT NULL,
+  `date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `goods`
+--
+ALTER TABLE `goods`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `goods`
+--
+ALTER TABLE `goods`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
